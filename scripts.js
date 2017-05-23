e5db0fc9b1e6a89b584785afe77fd39aa646877a
Molly Foster <mollyrfoster@gmail.com>
2017-05-22 19:59:18 -0600

    constuctor function, unique IDs and adding items to local storage

--- scripts.js
+++ scripts.js
@@ -1,32 +1,44 @@
 var ideaTitleInput = $('.title-input');
 var ideaBodyInput = $('.body-input');
-var qualityName = document.querySelector('.quality');
+var emptyIdeaCard = $('.idea-card');
+var storageArray = [];
 
+//
 //clear input fields
 function clearInput() {
   ideaTitleInput.val('');
   ideaBodyInput.val('');
 }
+
+// //adding card to local storage
+function constructNewIdea(title, body) {
+  this.title = title;
+  this.body = body;
+  this.id = Date.now();
+  this.quality = 'Swill';
+}
+
 //retrieve content from input fields
-function newIdeaCard(title, body) {
+function prependIdeaCard(newIdeaCard) {
   $('.bottom-section').prepend(`<section
     class="card-holder-section">
-      <article class="idea-card">
+      <article class="idea-card" id=${newIdeaCard.id}>
         <div class="idea-card-header">
-          <h2>${title}</h2>
+          <h2>${newIdeaCard.title}</h2>
           <button class="delete-button" type="button" name="button"></button>
         </div>
-        <div class="article-text-container">
-          <p>${body}</p>
+        <div contenteditable= 'true' class="article-text-container">
+          <p>${newIdeaCard.body}</p>
         </div>
         <div class="quality-control-container">
         <button class="upvote-button" type="button" name="button"></button>
         <button class="downvote-button" type="button" name="button"></button>
-        <p>quality: swill</p>
+        <p>quality: <span class="quality">${newIdeaCard.quality}</p>
         </div>
       </article>
     </section>`);
   clearInput();
+
 }
 
 //add new idea card on click
@@ -34,8 +46,36 @@ $('.save-button').on('click', function(event) {
   event.preventDefault();
   var ideaTitle = ideaTitleInput.val();
   var ideaBody = ideaBodyInput.val();
-  newIdeaCard(ideaTitle, ideaBody);
+  var newIdeaCard = new constructNewIdea(ideaTitle, ideaBody);
+  prependIdeaCard(newIdeaCard);
+  constructNewIdea();
+  storeIdeaCard(newIdeaCard);
+  retrieveIdeas(newIdeaCard);
+  // console.log(newIdeaCard.title);
 });
+
+//store unique ID
+function storeIdeaCard(newIdeaCard) {
+  localStorage.setItem(newIdeaCard.id, JSON.stringify(newIdeaCard));
+  // console.log(newIdeaCard.id);
+}
+
+//retrieve idea card
+function retrieveIdeas(newIdeaCard) {
+  if(localStorage.getItem(newIdeaCard.id)) {
+    var storedIdeas = localStorage.getItem(newIdeaCard.id);
+    var parsedIdeas =  JSON.parse(storedIdeas);
+    console.log(parsedIdeas);
+
+    parsedIdeas.forEach(function(newIdeaCard) {
+      var singleIdea = `<article>${newIdeaCard.title}</article>`;
+      emptyIdeaCard.append(singleIdea);
+    })
+  } else {
+    console.log('is this working?');
+  }
+}
+retrieveIdeas();
 
 //delete idea card from bottom section
 $('.bottom-section').on('click','button.delete-button', function() {
